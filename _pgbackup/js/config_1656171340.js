feefeb7fe38903f4fbb5caa883da05024fbb2b69
Átomos <jurandirshora@yahoo.com.br>
2022-06-26 14:57:44 -0300

    Atualizando tudo
    Nesta atualização, o projeto está com as pastas e os arquivos de configuração do MQTT

---
+++ _pgbackup/js/config_1656171340.js
@@ -0,0 +1,123 @@
+//exemplo de script HTML/JS que publicará/assinará tópicos no Console do Google Chrome
+//por JSH.
+// https://www.eclipse.org/paho/files/jsdoc/index.html https://www.eclipse.org/paho/files/jsdoc/Paho.MQTT.Client.html-->
+var wsbroker = "broker.mqttdashboard.com";  //mqtt websocket enabled broker
+var wsport = 8000 // port for above
+
+var client = new Paho.MQTT.Client(wsbroker, wsport,
+    "Atomos_" + parseInt(Math.random() * 100, 10));
+
+client.onConnectionLost = function (responseObject) {
+    console.log("Conexão perdida: " + responseObject.errorMessage);
+};
+
+client.onMessageArrived = function (message) {
+    console.log("Usuário:" + options.userName);
+    console.log("Tópico:", message.destinationName, '\nMenssagem:', message.payloadString);
+    // menssagem devolvida do broker e guardada na variavel vars -->
+    var vars = (message.payloadString);
+    document.getElementById('payload').innerHTML = vars;
+    // Condições -->
+    if (vars == "ON") {
+        console.log("Retorno: Ligado!");
+    } else if (vars == "OFF") {
+        console.log("Retorno: Desligado!");
+
+    } else if (vars == "Online") {
+        console.log("Retorno: Conectado!");
+
+    } else {
+        console.log("Retorno: Não faz nada!");
+    }
+};
+
+var options = {
+    timeout: 3,
+    useSSL: false,
+    userName: " Jurandir",
+    password: "1234",
+
+    onSuccess: function () {
+        console.log("Broker MQTT conectado");
+        // Conexão bem-sucedida; subscrever o nosso tópico, pode adicionar várias linhas destes
+        client.subscribe("cmnd/casa/jardim/POWER", { qos: 2 });
+
+        //use o abaixo se você quiser publicar em um tópico no connect
+        message = new Paho.MQTT.Message("Conectado!");
+        message.destinationName = "cmnd/casa/jardim/POWER";
+        client.send(message);
+
+        // chamado quando chega uma mensagem
+        function onMessageArrived(message) {
+
+            console.log("onMessageArrived:" + message.payloadString);
+
+        }
+
+    },
+    onFailure: function (message) {
+        console.log("Falha na conexão: " + message.errorMessage);
+    }
+};
+
+
+
+function init() {
+    client.connect(options);
+}
+
+
+function fun1() {
+    //use o abaixo se você quiser publicar em um tópico no connect
+
+    message = new Paho.MQTT.Message("OFF");
+    message.destinationName = "cmnd/casa/jardim/POWER";
+    client.send(message);
+
+    console.log("Variavel: " + message.payloadString);
+
+    //setTimeout(function() {
+    //console.clear();}, 20*1000);
+
+}
+
+function ligar() {
+    //use o abaixo se você quiser publicar em um tópico no connect
+
+    message = new Paho.MQTT.Message("OFF");
+    message.destinationName = "cmnd/casa/jardim/POWER";
+    client.send(message);
+
+    console.log("Variavel: " + message.payloadString);
+
+    //setTimeout(function() {
+    //console.clear();}, 20*1000);
+
+}
+
+
+function desligar() {
+    //use o abaixo se você quiser publicar em um tópico no connect
+
+    message = new Paho.MQTT.Message("ON");
+    message.destinationName = "cmnd/casa/jardim/POWER";
+    client.send(message);
+
+    console.log("Variavel: " + message.payloadString);
+
+    //setTimeout(function() {
+    //console.clear();}, 20*1000);
+
+}
+
+function exibe() {
+    var vars = "Menssagem";
+    document.getElementById('payload').innerHTML = vars;
+}
+
+function iniciar() {
+    init();
+    // exibe(); 
+    // fun1(); 
+
+}
